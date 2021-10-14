b5036ee779ed2bb7c5ccd54fe6ce858a2fc484e0
Aieychan01 <87831895+Aieychan01@users.noreply.github.com>
2021-10-14 04:28:21 -0300

    envio das atividades

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css';
+import Messege from './Messege'
+
+function App() {
+  return (
+    <div className="App">
+     <Messege />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Messege.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './App.css'
+ 
+export default class Messege extends Component {
+
+    render() {
+        return (
+            <div className='resultado'>
+                <h1>
+                    Olá mundo React
+                </h1>
+                <p>
+                    Esse é meu componente de classe :]
+                </p>
+            </div>
+        )
+    }
+}
